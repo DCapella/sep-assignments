1b80bc8667d3670b8302f1b1bf0e5249f80cad81
David Capella <D_Capella@yahoo.com>
2018-01-02 23:19:06 -0600

    added a push and pop feature and allowed to check if empty on mystack.rb ... updated init and allowed to add to the back of the que and get rid of the front of the queue and check if empty on myqueue.rb

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -5,14 +5,27 @@ class MyQueue
   def initialize
     @queue = Array.new
     @head = @queue[0]
+    @tail = @queue[-1]
   end
 
   def enqueue(element)
+    @queue << element
+    self.update_head_tail()
   end
 
   def dequeue
+    temp = @queue[0]
+    @queue.delete(temp)
+    self.update_head_tail()
   end
 
   def empty?
+    return true if @queue.length == 0
+    false
+  end
+
+  def update_head_tail
+    @head = @queue[0]
+    @tail = @queue[-1]
   end
 end
