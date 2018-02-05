fcd2bc9efabd7458cdf0bf7daf17966312c0eae2
David Capella <D_Capella@yahoo.com>
2018-02-05 12:18:47 -0600

    finished open_addressing

--- 01-data-structures/05-hashes-part-2/open_addressing/open_addressing.rb
+++ 01-data-structures/05-hashes-part-2/open_addressing/open_addressing.rb
@@ -2,22 +2,36 @@ require_relative 'node'
 
 class OpenAddressing
   def initialize(size)
+    @items = Array.new(size)
   end
 
   def []=(key, value)
+    node_item = Node.new(key, value)
+    node_index = self.index(key, @items.length)
+
+    if @items[node_index].nil?
+      @items[node_index] = node_item
+    elsif not nil in @items
+      self.resize
+    end
   end
 
   def [](key)
+    key_index = self.index(key, @items.length)
+    @items[key_index].value
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+    index = key.sum % size
   end
 
   # Given an index, find the next open index in @items
   def next_open_index(index)
+    return -1 if index + 1 >= @items.length
+    @items[index + 1].value
   end
 
   # Simple method to return the number of items in the hash
@@ -26,5 +40,12 @@ class OpenAddressing
 
   # Resize the hash
   def resize
+    temp_items = @items
+    @items = Array.new(temp_items.length * 2)
+    temp_items.each do |item|
+      if item != nil
+        self.[]=(item.key, item.value)
+      end
+    end
   end
 end
