aebc2cb89395cf062460abd1c8b1653f82a96d73
David Capella <D_Capella@yahoo.com>
2018-03-20 07:34:03 -0500

    finished binary tree and heap along with txt answers

--- 01-data-structures/06-trees/binary_heap/binary_heap_tree.rb
+++ 01-data-structures/06-trees/binary_heap/binary_heap_tree.rb
@@ -1,51 +1,72 @@
 require_relative 'node'
 
-class BinarySearchTree
+class BinaryHeapTree
 
   def initialize(root)
     @root = root
   end
 
-  def insert(root, node)
-    if root.rating < node.rating
-      if root.right.nil?
-        root.right = node
+  def insert(root, node, children=nil)
+    next_children = []
+    if children.nil?
+      children = []
+      children << root
+    end
+    children.each do |child|
+      if child.left.nil?
+        child.left = node
+        next_children = []
+        break
+      elsif child.right.nil?
+        child.right = node
+        next_children = []
+        break
+      elsif child.left.rating < node.rating
+        temp = child.left
+        child.left = node
+        next_children = []
+        self.insert(root, temp)
+      elsif child.right.rating < node.rating
+        temp = child.right
+        child.right = node
+        next_children = []
+        self.insert(root, temp)
       else
-        self.insert(root.right, node)
+        next_children << child.left
+        next_children << child.right
       end
-    else
-      if root.left.nil?
-        root.left = node
-      else
-        self.insert(root.left, node)
-      end
+    end
+    unless next_children.empty?
+      self.insert(root, node, next_children)
     end
   end
 
   # Recursive Depth First Search
   def find(root, data, delete=false)
+    output = nil
     unless root.left.nil?
       if root.left.title == data
         unless delete
-          return root.left
+          output = root.left
         else
           root.left = nil
         end
       else
-        return self.find(root.left, data, delete)
+        output = self.find(root.left, data, delete)
       end
     end
-    unless root.right.nil?
+    unless root.right.nil? || !output.nil?
       if root.right.title == data
         unless delete
-          return root.right
+          output = root.right
         else
           root.right = nil
         end
       else
-        return self.find(root.right, data, delete)
+        output = self.find(root.right, data, delete)
       end
     end
+    output
   end
 
   def delete(root, data)
